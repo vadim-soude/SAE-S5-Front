3a20857bea23f2a37ef89f32d54ff8ae303bce56
vadimsoude <vadimsoude@gmail.com>
2023-10-21 14:55:56 +0200

    - Refactoring of some code - Adding a function getTotalPrice - Adding a function to switch between pref price and normal price - Adding a function to get the price of product depending on the quantity of this product

--- src/app/pages/components/panier/panier.component.ts
+++ src/app/pages/components/panier/panier.component.ts
@@ -16,7 +16,11 @@ import {CacheService} from "../../../data/services/cache/cache.service";
 export class PanierComponent implements OnInit {
 
     products: IProductsModel[] = [];
+    isPref: boolean = false;
     productMap = new Map<IProductModel,number>();
+    totalPrice: number = 0;
+    totalPricePref: number = 0;
+    totalPriceDisplay: number = 0;
 
     constructor(private productService: ProductService, private cacheService : CacheService) { }
 
@@ -38,19 +42,16 @@ export class PanierComponent implements OnInit {
         for (let productMapElement of this.productMap) {
             this.products.push({product: productMapElement[0], quantity: productMapElement[1]});
         }
+        this.getTotalPrice();
     }
 
     modifyQuantity(productID: string, quantityChange: number):void{
-        let prod = this.products.find((element) => element.product.guid == productID)
-        let index = -1;
-        if (prod) {
-            index = this.products.indexOf(prod);
-        }
-        if(index != -1){
+        let index = this.getProductIndex(productID)
+        if(index != null){
             this.products[index].quantity = this.products[index].quantity + quantityChange;
-        }
-        if(this.products[index].quantity <= 0){
-            this.products.splice(index,1)
+            if(this.products[index].quantity <= 0){
+                this.products.splice(index,1)
+            }
         }
         this.setContent(this.products);
     }
@@ -67,11 +68,63 @@ export class PanierComponent implements OnInit {
             }
         }
         this.cacheService.set("panier",tempString);
+        this.getTotalPrice();
+    }
+
+    getTotalPrice(){
+        this.totalPrice = 0;
+        this.totalPricePref = 0;
+        for (const products of this.products) {
+            this.totalPrice = this.totalPrice + (products.quantity * products.product.price);
+            this.totalPricePref = this.totalPricePref + (products.quantity * products.product.pricePref);
+        }
+        if(this.isPref){
+            this.totalPriceDisplay = this.totalPricePref
+        }else{
+            this.totalPriceDisplay = this.totalPrice
+        }
+    }
+
+    debugSwitch():void{
+        if(this.isPref){
+            this.isPref = false;
+        }else{
+            this.isPref = true;
+        }
+        this.getTotalPrice()
     }
 
     clearProduct():void{
         this.cacheService.set("panier","");
         window.location.reload();
     }
+
+    getProductIndex(productGUID: string) : number | null{
+        let prod = this.products.find((element) => element.product.guid == productGUID)
+        let index = -1;
+        if (prod) {
+            index = this.products.indexOf(prod);
+        }
+        if(index != -1){
+            return index;
+        }else{
+            return null;
+        }
+    }
+
+    getProductPrice(product: IProductModel, isPref: boolean): number {
+        let index = this.getProductIndex(product.guid)
+        let quantity = 0;
+        if(index != null){
+            quantity = this.products[index].quantity
+        }
+        if(isPref){
+            return product.pricePref * quantity;
+        }else{
+            return product.price * quantity;
+        }
+    }
+
+
 
 }
