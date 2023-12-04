4a6e046d623f82b9d51670fbfd08c6e8eadb5bfe
vadimsoude <vadimsoude@gmail.com>
2023-12-04 10:43:24 +0100

    fix import

--- src/app/data/auth/guard/AuthGuard.ts
+++ src/app/data/auth/guard/AuthGuard.ts
@@ -1,6 +1,6 @@
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
-import { AuthService } from "./auth.service";
+import { AuthService } from "../auth.service";
 
 export const AuthGuard = () => {
 
